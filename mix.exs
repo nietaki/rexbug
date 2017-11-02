f51d0d514a276e365e4434765020f158daca8415
Jacek Królikowski <nietaki@gmail.com>
2017-11-02 14:56:55 +0000

    cleaning up leftover ast

--- mix.exs
+++ mix.exs
@@ -28,7 +28,7 @@ defmodule Rexbug.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     # Specify extra applications you'll use from Erlang/Elixir
-    [extra_applications: [:logger]]
+    [extra_applications: []]
   end
 
 
